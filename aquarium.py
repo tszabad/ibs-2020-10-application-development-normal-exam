2a414caf55b2aa6dde15af569604fca4adc55caa
Tamas Szabados <tszabad@gmail.com>
2021-01-20 17:44:38 +0100

    aquarium class is ready

--- aquarium.py
+++ aquarium.py
@@ -3,4 +3,21 @@ from fish import Fish, Tang, Clownfish, Kong,
 class Aquarium():
     def __init__(self):
         self.fishes = []
-        
+
+    def addFish(self, fish):
+        self.fishes.append(fish)
+
+    def feed(self):
+        for i in self.fishes:
+            i.feed()
+    
+    def removeFish(self):
+        for i in self.fishes:
+            if i.weight >= 11:
+                self.fishes.remove(i)
+
+    def get_status(self):
+        for i in self.fishes:
+            i.status()
+
+
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-
+from fish import Fish, Tang, Clownfish, Kong,
+from aquarium import Aquarium
 
 def main():
+    aquarium = Aquarium()
+    fish = Tang()
 
 
 
